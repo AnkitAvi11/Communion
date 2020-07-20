076a66f715a03bf64966fed0709a930cf9d6bd6a
AnkitKumar1311 <kumar.ankit383@gmail.com>
2020-07-21 01:08:35 +0530

    User profile section created

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
+    re_path(r'^@(?P<username>[a-zA-Z0-9\-\_\.]+)/$', views.viewUser, name='viewuser'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,21 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+
+from django.contrib.auth.models import User
+from django.core.serializers import serialize
 
 def homePage(request) : 
     if request.user.is_authenticated : 
         return render(request, 'pages/home.html')
     else : 
         return render(request, 'pages/index.html')
+
+def viewUser(request, username) :
+    username = username.split("@")[0]
+    if User.objects.filter(username=username).exists() : 
+        user = serialize("json", User.objects.filter(username=username))
+        return JsonResponse(user, safe=False)
+    else : 
+        raise Http404()
+
+    
