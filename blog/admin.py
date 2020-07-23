c98ce3d6cda6ecfc66c66a9ef3b4a43a09460f03
AnkitKumar1311 <kumar.ankit383@gmail.com>
2020-07-24 03:42:42 +0530

    Model done

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blog
+
+class BlogAdmin(admin.ModelAdmin) : 
+    list_display = ('id', 'title', 'created_on', 'description', 'body')
+    list_display_links = ('id', 'title')
+
+
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,17 @@
 from django.db import models 
 
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Blog(models.Model) : 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    title = models.TextField()
+    #   slug = models.TextField()
+    description = models.TextField(blank=False, null=False)
+    body = models.TextField()
+    cover_image = models.ImageField(upload_to = 'blogs/%Y/%m/', blank=True, null=True)
+    created_on = models.DateTimeField(default=timezone.now())
+
+
+    def __str__(self) : 
+        return self.title
